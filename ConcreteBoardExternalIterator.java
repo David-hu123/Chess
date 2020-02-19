caa0edf8ff5e1f86c26044eef1a38cfcf7d1c79d
David-hu123 <48746639+David-hu123@users.noreply.github.com>
2020-02-19 16:21:59 -0500

    Delete ConcreteBoardExternalIterator.java

--- ConcreteBoardExternalIterator.java
+++
@@ -1,6 +0,0 @@
-public class ConcreteBoardExternalIterator implements BoardExternalIterator {
-    @Override
-    public void visit(String loc, Piece p) {
-        System.out.println(loc+" is viewed, and the piece is "+p.toString());
-    }
-}
